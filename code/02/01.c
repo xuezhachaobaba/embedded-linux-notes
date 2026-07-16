7aae6a389cc0b6a2fa754ae52a4f96b2e2ca133b
zhuchao <710428516@qq.com>
2026-07-16 23:44:24 +0800

    first commit

---
+++ code/02/01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+	int a = 10;
+	int *p = &a;
+	*p = 20;
+	printf("%d\n",a);
+	return 0;
+}
---
+++ code/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void){
+	int x;
+	printf("Hello, Embedded Linux!\n");
+	return 0;
+}
